119c80aeeca90d0c67887361a8ef34cf5ba773bb
Ashok Kumar Rawat <ashok.rawat.blr@gmail.com>
2023-12-30 11:16:37 +0530

    fix

--- index-main.js
+++ index-main.js
@@ -96,7 +96,7 @@ app.get("/eml", function (req, res) {
   res.send(callerJoinToConfEML(req));
 
     // make call to Bot
-    var eml = '<?xml version="1.0" encoding="UTF-8"?><Response><Say> hi I am Bot speaking </Say><Dial><Conference>' + req.query.CallID + '</Conference></Dial></Response>'
+    var eml = '<Response><Dial><Conference>' + req.query.CallID + '</Conference></Dial></Response>'
     makeCallAPI(req.query.To, 'sip:123@rsys-test.sip.twilio.com', eml)
 });
 
